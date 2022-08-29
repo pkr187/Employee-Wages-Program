0966c37d7feb204aad798b174d0ec71781e223ba
pkr187 <pankajroman187@gmail.com>
2022-08-29 16:58:37 +0530

    Welcome to employee wages program

---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO EMPLOYEE WAGES PROGRAM");
+                
+        }
+    }
+}
