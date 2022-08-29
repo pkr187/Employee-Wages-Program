9e57c4ef0501ad0197150041c8de272424d93e3f
pkr187 <pankajroman187@gmail.com>
2022-08-29 17:40:26 +0530

    Calcu-Wages-Month

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -7,29 +7,31 @@ namespace EmployeeWage
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_Hrs = 20;
+        public const int NUM_OF_WORKING_DAYS = 2;
         static void main(String[] args)
         {
             //variable
-            int empHrs = 0;
-            int empWage = 0;
-            Random random = new Random();
+            int empHrs = 0, empWage = 0, totaleamWage = 0;
             //computation
-            int empcheck = random.Next(0, 3);
-            switch (empcheck)
+            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
             {
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                Random random = new Random();
+                int empcheck = random.Next(0, 3);
+                switch (empcheck)
+                {
+                    case IS_PART_TIME:
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_Hrs;
+                Console.WriteLine("Emp Wage : " + empWage!);
             }
-            empWage = empHrs * EMP_RATE_PER_Hrs;
-            Console.WriteLine("Emp Wage : " + empWage!);
+            Console.WriteLine("Total Emp Wage : " + totaleamWage);
         }
-
     }
 }
